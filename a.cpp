35bcb2fa541963b9305e6047408a6d67ac9b026c
Venom-61 <subhendukarmakar61@gmail.com>
2022-12-03 03:09:55 +0530

    Doremy's Paint

---
+++ a.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+// A. Doremy's Paint
+
+void solve() {
+    int n;
+    cin >> n;
+    int x;
+    for(int i = 1; i <= n; i++) {
+        cin >> x;
+    }
+
+    cout << 1 << " " << n << "\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}
